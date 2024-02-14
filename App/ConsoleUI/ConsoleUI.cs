b088d970299b90bc4fc879a8df13949b95991fa9
João Lacerda Oliveira <John.Lacerda.Oliveira@gmail.com>
2024-02-14 16:39:54 +0000

    Abstract Console as the UI

--- App/ConsoleUI/ConsoleUI.cs
+++ App/ConsoleUI/ConsoleUI.cs
@@ -1,6 +1,47 @@
 ﻿namespace Console_PhoneBook.App.ConsoleUI
 {
-    internal class ConsoleUI
+    public class ConsoleUI : IGenericUI
     {
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void PrintOptions(IEnumerable<string> options)
+        {
+            var counter = 1;
+            foreach (var option in options)
+            {
+                Console.WriteLine($"{counter++}. {option}");
+            }
+        }
+
+        public string GetUserInput()
+        {
+            string? userInput;
+            bool isValidInput = false;
+
+            do
+            {
+                userInput = Console.ReadLine();
+
+                if(userInput != null && userInput.Length > 0) isValidInput = true;
+
+
+            } while (!isValidInput);
+
+            return userInput;
+        }
+
+        public void ClearConsole()
+        {
+            Console.Clear();
+        }
+
+        public void PressKeyToContinue()
+        {
+            Console.ReadKey();
+        }
     }
 }
---
+++ App/ConsoleUI/IGenericUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_PhoneBook.App.ConsoleUI
+{
+    public interface IGenericUI
+    {
+        public void PrintMessage(string message);
+
+        public void PrintOptions(IEnumerable<string> options);
+
+        public string GetUserInput();
+
+        public void ClearConsole();
+
+        public void PressKeyToContinue();
+    }
+}
