405305cb077459195ec5915a8bd5136359ed5b65
João Lacerda Oliveira <John.Lacerda.Oliveira@gmail.com>
2024-03-07 00:07:22 +0000

    start implementing tests

---
+++ Unit Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Console_PhoneBook.App.Functionality;
+
+namespace Unit_Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
