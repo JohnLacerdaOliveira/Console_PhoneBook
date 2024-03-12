aa327e50942bc4cfdd284a7723ea4dc5576f4810
João Lacerda Oliveira <John.Lacerda.Oliveira@gmail.com>
2024-03-12 20:11:57 +0000

    alter location of test phoneBooks

--- Unit Tests/UnitTest1.cs
+++ Unit Tests/UnitTest1.cs
@@ -2,18 +2,9 @@ using Console_PhoneBook.App.Functionality;
 
 namespace Unit_Tests
 {
-    [TestFixture]
+  
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+  
     }
 }
